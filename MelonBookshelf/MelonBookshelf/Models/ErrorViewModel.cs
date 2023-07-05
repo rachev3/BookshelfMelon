5b84ed8aca6245150fbab4f74fcee5d901db8b2c
rachev3 <ddrachev123@gmail.com>
2023-07-05 11:01:19 +0300

    initial

---
+++ MelonBookshelf/MelonBookshelf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MelonBookshelf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
