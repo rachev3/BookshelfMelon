24314f5711a7bd0a993a828f541580f35234ddd0
Mariyana22 <138572349+Mariyana22@users.noreply.github.com>
2023-07-05 14:04:45 +0300

    resource

---
+++ MelonBookshelf/MelonBookshelf/Data/Services/ResourceService.cs
@@ -0,0 +1,49 @@
+﻿using MelonBookshelf.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MelonBookshelf.Data.Services
+{
+    public class ResourceService : IResourceService
+    {
+        private readonly ApplicationDbContext _appDbContext;
+
+        public ResourceService (ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task Add(Resource resource)
+        {
+            await _appDbContext.Resources.AddAsync(resource);
+            await _appDbContext.SaveChangesAsync();
+        }
+        public async Task Delete(int id)
+        {
+            var result = await _appDbContext.Resources.FirstOrDefaultAsync(n => n.ResourceId == id);
+            _appDbContext.Resources.Remove(result);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Resource>> GetAll()
+        {
+            var result = await _appDbContext.Resources.ToListAsync();
+            return result;
+        }
+
+        public async Task<Resource> GetById(int id)
+        {
+            var result = await _appDbContext.Resources.FirstOrDefaultAsync(n => n.ResourceId == id);
+            return result;
+        }
+
+        public async Task<Resource> Update(int id, Resource resource)
+        {
+            _appDbContext.Update(resource);
+            await _appDbContext.SaveChangesAsync();
+            return resource;
+        }
+
+    }
+}
+
