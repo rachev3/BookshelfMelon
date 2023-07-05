3f5ddd6778258fd4a42ac47841ead29a29df2ad5
rachev3 <ddrachev123@gmail.com>
2023-07-05 14:14:17 +0300

    upvote,user,wanted :: I,service,Controller

---
+++ MelonBookshelf/MelonBookshelf/Controllers/WantedResourcesController.cs
@@ -0,0 +1,78 @@
+﻿using MelonBookshelf.Data.Services;
+using MelonBookshelf.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MelonBookshelf.Controllers
+{
+    public class WantedResourcesController : Controller
+    {
+        private readonly IWantedResourcesService wantedResourcesService;
+        public WantedResourcesController(IWantedResourcesService wantedResourcesService)
+        {
+            this.wantedResourcesService = wantedResourcesService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await wantedResourcesService.GetAll();
+            return View("WantedResources", data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(WantedResources wantedResources)
+        {
+            await wantedResourcesService.Add(wantedResources);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var wantedResources = await wantedResourcesService.GetById(id);
+            if (wantedResources == null)
+            {
+                return View("NotFound");
+            }
+            return View(wantedResources);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, WantedResources wantedResources)
+        {
+            wantedResources.Id = id;
+            if (!ModelState.IsValid)
+            {
+                return View(wantedResources);
+            }
+            await wantedResourcesService.Update(id, wantedResources);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var wanted = await wantedResourcesService.GetById(id);
+            if (wanted == null)
+            {
+                return View("NotFound");
+            }
+            return View(wanted);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var wantedResources = await wantedResourcesService.GetById(id);
+            if (wantedResources == null)
+            {
+                return View("NotFound");
+            }
+
+            await wantedResourcesService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MelonBookshelf/MelonBookshelf/Data/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using MelonBookshelf.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MelonBookshelf.Data.Services
+{
+    public class UserService:IUserService
+    {
+        private readonly ApplicationDbContext _appDbContext;
+
+        public UserService(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task Add(User user)
+        {
+            await _appDbContext.Users.AddAsync(user);
+            await _appDbContext.SaveChangesAsync();
+        }
+        public async Task Delete(string id)
+        {
+            var result = await _appDbContext.Users.FirstOrDefaultAsync(n => n.Id == id);
+            _appDbContext.Users.Remove(result);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            var result = await _appDbContext.Users.ToListAsync();
+            return result;
+        }
+
+        public async Task<User> GetById(string id)
+        {
+            var result = await _appDbContext.Users.FirstOrDefaultAsync(n => n.Id == id);
+            return result;
+        }
+
+        public async Task<User> Update(string id, User user)
+        {
+            _appDbContext.Update(user);
+            await _appDbContext.SaveChangesAsync();
+            return user;
+        }
+
+    }
+}
