ae8f4e7a0a17c10c6ea970555e5f3ac8c3d64a32
rachev3 <ddrachev123@gmail.com>
2023-07-31 13:55:49 +0300

    alert removed

--- MelonBookshelf/MelonBookshelf/wwwroot/js/site.js
+++ MelonBookshelf/MelonBookshelf/wwwroot/js/site.js
@@ -22,20 +22,9 @@ function toggleDarkTheme() {
     var button = document.getElementById('darkThemeButton');
     button.classList.toggle("dark-theme");
 
-    // Check if the button is currently in the dark-theme state and update the cookie accordingly
-    //if (button.classList.contains("dark-theme")) {
-    //    document.cookie = "DarkTheme=Dark;path=/";
-    //} else {
-    //    document.cookie = "DarkTheme=;path=/";
-    //}
-
-    // Make a POST request to the server to update the DarkTheme cookie
-
     $.ajax({
         url: "/User/DarkTheme",
         type: "POST",
-        success: function (response) {
-            alert(response);
-        }
+
     });
 }
