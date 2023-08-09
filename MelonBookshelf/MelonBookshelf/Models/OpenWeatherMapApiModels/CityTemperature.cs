b6d0b76e1d7b6b7b9c09db54e528d653b21dc754
rachev3 <ddrachev123@gmail.com>
2023-08-09 13:26:22 +0300

    icon of weather working

--- MelonBookshelf/MelonBookshelf/Models/OpenWeatherMapApiModels/CityTemperature.cs
+++ MelonBookshelf/MelonBookshelf/Models/OpenWeatherMapApiModels/CityTemperature.cs
@@ -1,4 +1,6 @@
-﻿namespace MelonBookshelf.Models
+﻿using static System.Net.WebRequestMethods;
+
+namespace MelonBookshelf.Models
 {
     public class CityTemperature
     {
@@ -6,8 +8,10 @@
         {
             CityName = weatherData.Name;
             Temperature = weatherData.Main.TempCelsius;
+            Icon = "https://openweathermap.org/img/wn/" + weatherData.Weather[0].Icon + ".png";
         }
         public string CityName { get; set; }
         public float Temperature { get; set; }
+        public string Icon { get; set; }
     }
 }
