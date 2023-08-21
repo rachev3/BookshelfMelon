c446f14953632fe3e248b9e4ce567814954ad6fb
rachev3 <ddrachev123@gmail.com>
2023-08-21 13:53:45 +0300

    viewModels for replies

---
+++ MelonBookshelf/MelonBookshelf/Models/CommentReplyModels/CommentReplyPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MelonBookshelf.Models.CommentReplyModels
+{
+    public class CommentReplyPageViewModel
+    {
+        public CommentReplyPageViewModel(List<CommentReplyViewModel> commentReplies)
+        {
+            CommentReplies = commentReplies;
+        }
+
+        public List<CommentReplyViewModel> CommentReplies { get; set; }
+    }
+}
---
+++ MelonBookshelf/MelonBookshelf/Models/CommentReplyModels/CommentReplyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MelonBookshelf.Data.DTO;
+
+namespace MelonBookshelf.Models.CommentReplyModels
+{
+    public class CommentReplyViewModel
+    {
+        public CommentReplyViewModel()
+        {
+                
+        }
+
+        public CommentReplyViewModel(CommentReply commentReply)
+        {
+            ReplyId = commentReply.ReplyId;
+            UserId = commentReply.UserId;
+            User = commentReply.User;
+            ResourceCommentId = commentReply.ResourceCommentId;
+            Reply = commentReply.Reply;
+        }
+
+        public int ReplyId { get; set; } 
+        public string UserId { get; set; }
+        public User? User { get; set; }
+        public int ResourceCommentId { get; set; }
+        public string Reply {  get; set; }
+    }
+}
